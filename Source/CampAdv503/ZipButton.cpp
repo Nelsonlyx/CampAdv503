84d72c3eef81f934a0e04a4b1400515f3586861d
Nelsonlyx <neslon.lee.yaoxiong@gmail.com>
2023-01-03 18:27:33 +0800

    replaced zipgrabhandle with zipbutton instead

---
+++ Source/CampAdv503/ZipButton.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ZipButton.h"
+
+// Sets default values
+AZipButton::AZipButton()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AZipButton::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AZipButton::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
